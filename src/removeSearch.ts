7175d303303d13391dc6aa2b6523742458268251
Mike Lysikov <d8corp@mail.ru>
2020-05-28 19:45:02 +0300

    deprecated removeSearch

--- src/removeSearch.ts
+++ src/removeSearch.ts
@@ -1,7 +1,12 @@
 import parseUrl from './parseUrl'
 
-export default function removeSearch (url: string, key: string): string {
+/**
+ * @deprecated Please use `setSearch` with 2 arguments
+ */
+function removeSearch (url: string, key: string): string {
   const {path = '', search = '', hash = ''} = parseUrl(url)
   const newSearch = search.replace(new RegExp(`(^|&)${key}(=[^&]*)?(&|$)`), '&').replace(/(^&|&$)/, '')
   return path + (newSearch ? '?' + newSearch : '') + (hash ? '#' + hash : '')
 }
+
+export default removeSearch
