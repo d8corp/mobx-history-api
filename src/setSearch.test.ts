8bf36dd757d2b748f8e81568764f01c464df3f40
Mike Lysikov <d8corp@mail.ru>
2020-05-29 15:48:25 +0300

    updated tests

--- src/setSearch.test.ts
+++ src/setSearch.test.ts
@@ -1,10 +1,32 @@
 import setSearch from './setSearch'
 
 describe('setSearch', () => {
-  it('simple', () => {
+  it('remove', () => {
+    expect(setSearch('', 'key')).toBe('')
+    expect(setSearch('?', 'key')).toBe('')
+    expect(setSearch('?key', 'key')).toBe('')
+    expect(setSearch('?key=value', 'key')).toBe('')
+    expect(setSearch('?test=value', 'key')).toBe('?test=value')
+    expect(setSearch('?test=value&key=value', 'key')).toBe('?test=value')
+    expect(setSearch('?test&key=value', 'key')).toBe('?test')
+    expect(setSearch('?key=value&test&', 'key')).toBe('?test&')
+    expect(setSearch('key', 'key')).toBe('key')
+
+    expect(setSearch('/test', 'key')).toBe('/test')
+    expect(setSearch('/test?', 'key')).toBe('/test')
+    expect(setSearch('/test?key', 'key')).toBe('/test')
+    expect(setSearch('/test?key=value', 'key')).toBe('/test')
+    expect(setSearch('/test?test=value', 'key')).toBe('/test?test=value')
+    expect(setSearch('/test?test=value&key=value', 'key')).toBe('/test?test=value')
+    expect(setSearch('/test?test&key=value', 'key')).toBe('/test?test')
+    expect(setSearch('/test?key=value&test&', 'key')).toBe('/test?test&')
+    expect(setSearch('/test/key', 'key')).toBe('/test/key')
+  })
+  it('set', () => {
     expect(setSearch('/', 'key', '')).toBe('/?key')
     expect(setSearch('/', 'key', 'value')).toBe('/?key=value')
     expect(setSearch('', 'key', 'value')).toBe('?key=value')
+    expect(setSearch('?', 'key', 'value')).toBe('?key=value')
     expect(setSearch('?test=checked', 'key', 'value')).toBe('?test=checked&key=value')
     expect(setSearch('/path/1#test', 'key', 'value')).toBe('/path/1?key=value#test')
     expect(setSearch('/path/1?key', 'key', 'value')).toBe('/path/1?key=value')
